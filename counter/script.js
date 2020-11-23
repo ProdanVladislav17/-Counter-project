8a9c2c8d0b08fc26f741468f836d70e220f93660
PRODAN-VLADISLAV <45701835+ProdanVladislav17@users.noreply.github.com>
2020-11-23 20:47:52 +0200

    Counter-project

---
+++ counter/script.js
@@ -0,0 +1,17 @@
+let addBtn = document.querySelector('#add');
+let subBtn = document.querySelector('#sub');
+let qty = document.querySelector('#qtyBox');
+
+addBtn.addEventListener('click',()=> {
+    qty.value =parseInt(qty.value) + 1;
+
+});
+
+subBtn.addEventListener('click',()=>{
+    if(qty.value <= 0){
+        qty.value = 0;
+    }
+    else{
+        qty.value = parseInt(qty.value) - 1;
+    }
+});
